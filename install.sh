5a69282d6e1d24eb7f63ed7469babeeee9545154
Giovanni <giovanni_neves@tutanota.com>
2022-05-19 17:52:21 -0300

    update installer

--- install.sh
+++ install.sh
@@ -1,39 +1,40 @@
-#! /bin/sh 
-DOTHOME=$HOME/dotfiles/home/
-DOTCONFIG=$HOME/dotfiles/.config/
+#! /bin/sh
+DOTHOME=$HOME/dotfiles_bspwm/home/
+DOTCONFIG=$HOME/dotfiles_bspwm/.config/
 
 # INSTALL ALL DEPENDENCES
 sudo xbps-install -Suy arandr base-devel bash-completion bspwm dmenu feh git htop lxappearance mpv neofetch neovim nerd-fonts NetworkManager nitrogen picom polybar pulseaudio python3 python3-pip rxvt-unicode sxhkd Thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman xdg-user-dirs xorg xrandr zsh &&
-clear && echo "Dependences installed!"
+    clear && echo "Dependences installed!"
 
 # CREATE USER'S DIRECTORY
 xdg-user-dirs-update
 echo "User's default dirs created!"
 
 # COPY DOTFILE/HOME TO SYSTEM $HOME
-cp $DOTHOME/.Xresources $DOTHOME/.bashrc $DOTHOME/.xinitrc $HOME
-cp $DOTHOME/.wallpaper/*.jpg $HOME/.wallpaper/ & mkdir -p $HOME/.wallpaper 
+cp "$DOTHOME"/.Xresources "$DOTHOME"/.bashrc "$DOTHOME"/.xinitrc "$HOME"
+cp "$DOTHOME"/.wallpaper/*.jpg "$HOME"/.wallpaper/ &
+mkdir -p "$HOME"/.wallpaper
 echo "\$HOME copy!"
 
 # Apply Xresources
-xrdb $HOME/.Xresources
+xrdb "$HOME"/.Xresources
 echo "Xresources configurate!"
 
-# COPY DOTFILES/.CONFIG FILES TO THE SYSTEM'S $HOME/.CONFIG
-mkdir -p $HOME/.config/bspwm/ $HOME/.config/sxhkd/ $HOME/.config/polybar/ $HOME/.config/picom
+# COPY DOTFILES/.CONFIG FILES TO THE SYSTEM'S "$HOME"/.CONFIG
+mkdir -p "$HOME"/.config/bspwm/ "$HOME"/.config/sxhkd/ "$HOME"/.config/polybar/ "$HOME"/.config/picom
 echo "Created folders!"
-cp $DOTCONFIG/bspwm/bspwmrc $HOME/.config/bspwm/
-cp $DOTCONFIG/sxhkd/sxhkdrc $HOME/.config/sxhkd/
-cp $DOTCONFIG/polybar/config.ini $HOME/.config/polybar/
-cp $DOTCONFIG/polybar/launch.sh $HOME/.config/polybar/
-chmod +x $HOME/.config/polybar/launch.sh
-cp $DOTCONFIG/picom/picom.conf $HOME/.config/picom/
+cp "$DOTCONFIG"/bspwm/bspwmrc "$HOME"/.config/bspwm/
+cp "$DOTCONFIG"/sxhkd/sxhkdrc "$HOME"/.config/sxhkd/
+cp "$DOTCONFIG"/polybar/config.ini "$HOME"/.config/polybar/
+cp "$DOTCONFIG"/polybar/launch.sh "$HOME"/.config/polybar/
+chmod +x "$HOME"/.config/polybar/launch.sh
+cp "$DOTCONFIG"/picom/picom.conf "$HOME"/.config/picom/
 echo ".Config copy!"
 
 # ZSH
-chsh -s /bin/zsh 
 
-echo 'Please run: sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
-zsh
+echo "Please run: chsh -s /bin/zsh"
+echo "Please run: sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
 echo "_________________________________________"
 echo "Your base system is installed! Now you can run 'startx' or reboot and login to get into"
+zsh
